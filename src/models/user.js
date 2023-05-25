c098de6db82edd31fc0cb35efdff82c3889e6604
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-05-25 15:26:02 +0700

    complete all model

--- src/models/user.js
+++ src/models/user.js
@@ -42,5 +42,49 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
 
+  User.associate = models => {
+    User.hasMany(models.Post, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false
+      },
+      onDelete: 'RESTRICT'
+    });
+
+    User.hasMany(models.Comment, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false
+      },
+      onDelete: 'RESTRICT'
+    });
+
+    User.hasMany(models.Like, {
+      foreignKey: {
+        name: 'userId',
+        allowNull: false
+      },
+      onDelete: 'RESTRICT'
+    });
+
+    User.hasMany(models.Friend, {
+      as: 'Requester',
+      foreignKey: {
+        name: 'requesterId',
+        allowNull: false
+      },
+      onDelete: 'RESTRICT'
+    });
+
+    User.hasMany(models.Friend, {
+      as: 'Receiver',
+      foreignKey: {
+        name: 'receiverId',
+        allowNull: false
+      },
+      onDelete: 'RESTRICT'
+    });
+  };
+
   return User;
 };
