f64b92b10f19b8003ac262c760d8a72046b26c95
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-05-25 12:41:33 +0700

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          is: true
+        }
+      },
+      mobile: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          is: /^[0-9]{10}$/
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      profileImage: DataTypes.STRING,
+      coverImage: DataTypes.STRING
+    },
+    {
+      underscored: true
+    }
+  );
+
+  return User;
+};
